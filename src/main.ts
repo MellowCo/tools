f7ddaa612503db0752b216ed4ca85e35316801e9
MellowCo <799478052@qq.com>
2022-08-22 16:42:37 +0800

    chore(style): remove reset css

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '~/router'
 
-import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
-
-// 解决tailwind的reset样式冲突
-// https://www.naiveui.com/zh-CN/os-theme/docs/style-conflict
-const meta = document.createElement('meta')
-meta.name = 'naive-ui-style'
-document.head.appendChild(meta)
 
 const app = createApp(App)
 
