c8b8dc13746a130a70737e188f673c951976cce3
MellowCo <799478052@qq.com>
2022-09-03 14:46:49 +0800

    feat(text): json 编辑器

--- src/pages/text/json/index.vue
+++ src/pages/text/json/index.vue
@@ -1,13 +1,36 @@
 <script lang="ts" setup>
+import { JSONEditor } from 'vanilla-jsoneditor'
 
+const jsonRef = ref(null)
+
+const jsoneditor = ref()
+
+onMounted(() => {
+  jsoneditor.value = new JSONEditor({
+    target: jsonRef.value as any,
+    props: {
+      mode: 'text' as any,
+    },
+  })
+})
 </script>
 
 <template>
-  <div>
-    json
-  </div>
+  <div ref="jsonRef" class="absolute w-full h-full customer-theme" :class="{ 'jse-theme-dark': isDark }" />
 </template>
 
-<style lang="scss" scoped>
+<style lang="less" scoped>
+@import "vanilla-jsoneditor/themes/jse-theme-dark.css";
 
+.jse-theme-dark{
+  &.customer-theme{
+    --jse-theme-color: #18181c;
+    --jse-theme-color-highlight: #333333 ;
+  }
+}
+
+.customer-theme{
+  --jse-theme-color: #3883fa;
+  --jse-theme-color-highlight: #5f9dff;
+}
 </style>
