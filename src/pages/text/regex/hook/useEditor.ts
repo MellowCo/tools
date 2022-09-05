5f3964cd859017fe242affb7d4c1e2461591bc9f
MellowCo <799478052@qq.com>
2022-09-06 00:00:29 +0800

    feat(text/regex): no global mode

--- src/pages/text/regex/hook/useEditor.ts
+++ src/pages/text/regex/hook/useEditor.ts
@@ -22,57 +22,27 @@ export default function (regexStr: Ref<string>, regexModifierArr: Ref<string[]>,
 
   function regexMatch() {
     inputStatus.value = undefined
+
     let deltaDecorations: monaco.editor.IModelDeltaDecoration[] = []
 
     const isGlobal = regexModifierArr.value.includes('g')
 
     try {
       const regex = new RegExp(regexStr.value, regexModifier.value)
+      const cacheMap: Record<string, number> = {}
+
       if (isGlobal) {
         // 全局匹配
         const matchs = Array.from(editor.getValue().matchAll(regex) || [])
-        const cacheMap: Record<string, number> = {}
 
         matchs.forEach((match) => {
-          const [matchStr, ...groupStrs] = match
-
-          if (cacheMap[matchStr])
-            return
-
-          cacheMap[matchStr] = 1
-
-          const ranges = editor.getModel()?.findMatches(matchStr, true, false, true, null, true)
-          const _deltaDecorations: monaco.editor.IModelDeltaDecoration[] = []
-
-          ranges?.forEach(({ range }) => {
-            const { startLineNumber, startColumn, endLineNumber, endColumn } = range
-
-            let startIndex = startColumn
-
-            groupStrs.forEach((groupStr, index) => {
-              if (!groupStr)
-                return
-              // 计算 group 的位置
-              const groupStart = matchStr.indexOf(groupStr)
-              const groupStartColumn = startColumn + groupStart
-              const groupEndColumn = groupStartColumn + groupStr.length
-
-              // 设置 group 背景
-              _deltaDecorations.push(createDeltaDecoration(startLineNumber, groupStartColumn, endLineNumber, groupEndColumn, groupBgClass[index % groupBgClass.length]))
-
-              // 设置 macth 背景
-              _deltaDecorations.push(createDeltaDecoration(startLineNumber, startIndex, endLineNumber, groupStartColumn, 'regex-match-bg'))
-
-              startIndex = groupEndColumn
-            })
-
-            _deltaDecorations.push(createDeltaDecoration(startLineNumber, startIndex, endLineNumber, endColumn, 'regex-match-bg'))
-          })
-
-          deltaDecorations = [...deltaDecorations, ..._deltaDecorations]
+          deltaDecorations = [...deltaDecorations, ...createDeltaDecorations(match, cacheMap)]
         })
       }
-      // else { matchs = Array.from(editor.getValue().match(regex) || []) }
+      else {
+        const matchs = Array.from(editor.getValue().match(regex) || [])
+        deltaDecorations = createDeltaDecorations(matchs, cacheMap, false)
+      }
     }
     catch (e) {
       inputStatus.value = 'error'
@@ -88,6 +58,60 @@ export default function (regexStr: Ref<string>, regexModifierArr: Ref<string[]>,
         className,
       },
     }
+  }
+
+  // 计算正则匹配的位置
+  function calDeltaPos(range: monaco.Range, matchStr: string, groupStrs: string[]) {
+    const { startLineNumber, startColumn, endLineNumber, endColumn } = range
+    const _deltaDecorations: monaco.editor.IModelDeltaDecoration[] = []
+    let startIndex = startColumn
+
+    groupStrs.forEach((groupStr, index) => {
+      if (!groupStr)
+        return
+
+      // 计算 group 的位置
+      const groupStart = matchStr.indexOf(groupStr)
+      const groupStartColumn = startColumn + groupStart
+      const groupEndColumn = groupStartColumn + groupStr.length
+
+      // 设置 group 背景
+      _deltaDecorations.push(createDeltaDecoration(startLineNumber, groupStartColumn, endLineNumber, groupEndColumn, groupBgClass[index % groupBgClass.length]))
+
+      // 设置 macth 背景
+      _deltaDecorations.push(createDeltaDecoration(startLineNumber, startIndex, endLineNumber, groupStartColumn, 'regex-match-bg'))
+
+      startIndex = groupEndColumn
+    })
+
+    _deltaDecorations.push(createDeltaDecoration(startLineNumber, startIndex, endLineNumber, endColumn, 'regex-match-bg'))
+
+    return _deltaDecorations
+  }
+
+  function createDeltaDecorations(match: string[], cacheMap: Record<string, number>, isGlobal = true) {
+    let deltaDecorations: monaco.editor.IModelDeltaDecoration[] = []
+
+    const [matchStr, ...groupStrs] = match
+
+    if (cacheMap[matchStr] || !matchStr)
+      return []
+
+    cacheMap[matchStr] = 1
+
+    const ranges = editor.getModel()?.findMatches(matchStr, true, false, true, null, true)
+
+    if (isGlobal) {
+      ranges?.forEach(({ range }) => {
+        deltaDecorations = [...deltaDecorations, ...calDeltaPos(range, matchStr, groupStrs)]
+      })
+    }
+    else {
+      if (ranges?.length)
+        deltaDecorations = [...deltaDecorations, ...calDeltaPos(ranges[0].range, matchStr, groupStrs)]
+    }
+
+    return deltaDecorations || []
   }
 
   return {
